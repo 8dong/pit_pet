867493ae2583ed5d04ae8a43603e862b7ad48a31
DongHyun <hyde981114@gmail.com>
2022-10-19 02:06:37 +0900

    kakao map

--- components/Map.js
+++ components/Map.js
@@ -2,22 +2,23 @@ import { useEffect } from 'react';
 
 const Map = () => {
   useEffect(() => {
-    const script = document.createElement('script');
-    script.async = true;
-    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOMAP_APPKEY}&autoload=false`;
-    document.head.appendChild(script);
+    const mapScript = document.createElement('script');
+    mapScript.async = true;
+    // mapScript.type = 'text/js';
+    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOMAP_APPKEY}&autoload=false`;
+    document.head.appendChild(mapScript);
 
-    script.addEventListener('load', () => {
+    mapScript.addEventListener('load', () => {
       window.kakao.maps.load(() => {
         const mapContainer = document.getElementById('map');
+
         const mapOption = {
           center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-          level: 3 // 지도의 확대 레벨
+          level: 3, // 지도의 확대 레벨,
+          scrollwheel: false
         };
-        const map = new window.kakao.maps.Map(mapContainer, mapOption);
 
-        // 확대/축소 금지
-        map.setZoomable(false);
+        const map = new window.kakao.maps.Map(mapContainer, mapOption);
 
         // 마커가 표시될 위치입니다
         const markerPosition = new kakao.maps.LatLng(33.450701, 126.570667);
@@ -32,6 +33,34 @@ const Map = () => {
       });
     });
   }, []);
+
+  // useEffect(() => {
+  //   script.addEventListener('load', () => {
+  //     window.kakao.maps.load(() => {
+  //       const mapContainer = document.getElementById('map');
+
+  //       const mapOption = {
+  //         center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+  //         level: 3 // 지도의 확대 레벨
+  //       };
+  //       const map = new window.kakao.maps.Map(mapContainer, mapOption);
+
+  //       // 확대/축소 금지
+  //       map.setZoomable(false);
+
+  //       // 마커가 표시될 위치입니다
+  //       const markerPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+
+  //       // 마커를 생성합니다
+  //       const marker = new kakao.maps.Marker({
+  //         position: markerPosition
+  //       });
+
+  //       // 마커가 지도 위에 표시되도록 설정합니다
+  //       marker.setMap(map);
+  //     });
+  //   });
+  // }, []);
 
   return (
     <div
