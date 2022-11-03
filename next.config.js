12c45324ee42ced8bf2bb2c5715ddf655d851947
DongHyun <hyde981114@gmail.com>
2022-11-04 00:54:23 +0900

    image domains

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['media.istockphoto.com', 'i.imgur.com', 'cdn.pixabay.com', 'ssl.pstatic.net']
+    domains: [
+      'media.istockphoto.com',
+      'i.imgur.com',
+      'cdn.pixabay.com',
+      'ssl.pstatic.net',
+      'k.kakaocdn.net',
+      'googleusercontent.com'
+    ]
   }
 };
 
