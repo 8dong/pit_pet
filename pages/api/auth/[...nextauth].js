7e9c099d887c4d3efc73a1e2cab9de13f8b6fd1d
DongHyun <hyde981114@gmail.com>
2022-11-03 20:03:22 +0900

    Naver OAuth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,14 +4,41 @@ import clientPromise from '../../../lib/mongodb';
 import NaverProvider from 'next-auth/providers/naver';
 
 export const authOptions = {
-  adapter: MongoDBAdapter(clientPromise),
+  // adapter: MongoDBAdapter(clientPromise),
   providers: [
     NaverProvider({
       clientId: process.env.NAVER_CLIENT_ID,
       clientSecret: process.env.NAVER_CLIENT_SECRET
     })
   ],
-  secret: process.env.SECRET
+  secret: process.env.SECRET,
+  session: {
+    strategy: 'jwt'
+  }
+  // callbacks: {
+  //   async signIn({ user, account, profile }) {
+  //     console.log(user, account, profile);
+  //     console.log('signIn Callback');
+
+  //     return true;
+  //   },
+
+  //   async jwt({ token, user, account, profile, isNewUser }) {
+  //     try {
+  //       console.log(token, user, account, profile, isNewUser);
+  //       console.log('jwt callback');
+  //       return token;
+  //     } catch (err) {
+  //       consoole.log(err);
+  //     }
+  //   },
+
+  //   async session({ session, token, user }) {
+  //     console.log(session, token, user);
+  //     console.log('session callback');
+  //     return session;
+  //   }
+  // }
 };
 
 export default NextAuth(authOptions);
