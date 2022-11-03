74f37d52a151561a800f1c6abc237835123e0e3f
DongHyun <hyde981114@gmail.com>
2022-11-04 00:00:43 +0900

    scaleable=0

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,10 @@ class MyDocument extends Document {
           <title>PIT PET 애견 미용 스타일 가이드</title>
           <meta name='keywords' content='pet, pet style, 애견 스타일, 애견 미용' />
           <meta name='description' content='트렌디한 애견 미용 스타일 가이드북' />
+          <meta
+            name='viewport'
+            content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scaleable=0'
+          />
           <meta name='theme-color' content='#ffffff' />
         </Head>
         <body>
