d18a6ec808e7302d392432a02483ce27856786e1
DongHyun <hyde981114@gmail.com>
2022-11-10 05:47:41 +0900

    //

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import Router from 'next/router';
 import { SessionProvider } from 'next-auth/react';
 
-import Header from '../components/Layout/Header';
+import Head from 'next/head';
 
 import HomePageSkeleton from '../components/skeletonUI/HomePageSkeleton';
 import StyleListSkeleton from '../components/skeletonUI/StyleListSkeleton';
@@ -29,16 +29,16 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
 
   return (
     <SessionProvider session={session}>
-      <Header>
+      <Head>
         <title>PIT PET 애견 미용 스타일 가이드</title>
         <meta name='keywords' content='pet, pet style, 애견 스타일, 애견 미용' />
         <meta name='description' content='트렌디한 애견 미용 스타일 가이드북' />
         <meta
           name='viewport'
-          content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scaleable=no'
+          content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scaleable=0'
         />
         <meta name='theme-color' content='#ffffff' />
-      </Header>
+      </Head>
       <div className='page-section'>{pageContent}</div>
     </SessionProvider>
   );
